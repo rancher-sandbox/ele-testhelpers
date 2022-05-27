51a007145d2f2fe6545bd61d7b1cbca0dfc91ffd
Francesco Giudici <francesco.giudici@suse.com>
2022-05-27 11:19:30 +0200

    Move SUT and systemd helper
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

---
+++ vm/systemd.go
@@ -0,0 +1,16 @@
+package vm
+
+import (
+	"fmt"
+
+	. "github.com/onsi/gomega" //nolint:revive
+)
+
+func SystemdUnitIsStarted(s string, st *SUT) {
+	out, _ := st.Command(fmt.Sprintf("systemctl status %s", s))
+
+	Expect(out).To(And(
+		ContainSubstring(fmt.Sprintf("%s.service; enabled", s)),
+		ContainSubstring("status=0/SUCCESS"),
+	))
+}
