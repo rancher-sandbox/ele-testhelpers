06fe0feaa31e11d99368b625f3840644590e1bfc
Julien Adamek <julien.adamek@suse.com>
2023-10-16 16:37:42 +0200

    Create userCreate function
    Signed-off-by: Julien Adamek <julien.adamek@suse.com>

--- tools/tools_test.go
+++ tools/tools_test.go
@@ -12,6 +12,19 @@ import (
 	"github.com/rancher-sandbox/ele-testhelpers/tools"
 )
 
+func createUser() {
+	userName := "testuser"
+	err := exec.Command("sudo", "useradd", userName).Run()
+	Expect(err).To(Not(HaveOccurred()))
+
+	// Use 'sed' instead of 'tools.Sed' because root access
+	// is needed and it's easier with 'sudo'
+	userShadow := userName + ":$6$X7HdGuscUQ.XW6dW$B8rTHpY2bZJKyPebFn20fuj0oiLj3D9L557tTBbZ2ZycuIT23UOnjxwgQEki3//wK0/RKmXeOYPHbYhregyfu1:19122:0:99999:7:::"
+	regex := "s|^" + userName + ":.*|" + userShadow + "|"
+	err = exec.Command("sudo", "sed", "-i", regex, "/etc/shadow").Run()
+	Expect(err).To(Not(HaveOccurred()))
+}
+
 var _ = Describe("Tools tests", func() {
 	It("Test tools helpers functions", func() {
 		By("Testing GetFileFromURL function", func() {
@@ -88,15 +101,7 @@ var _ = Describe("Tools tests", func() {
 			Expect(err).To(HaveOccurred())
 
 			// Add 'testuser'
-			err = exec.Command("sudo", "useradd", userName).Run()
-			Expect(err).To(Not(HaveOccurred()))
-
-			// Use 'sed' instead of 'tools.Sed' because root access
-			// is needed and it's easier with 'sudo'
-			userShadow := userName + ":$6$X7HdGuscUQ.XW6dW$B8rTHpY2bZJKyPebFn20fuj0oiLj3D9L557tTBbZ2ZycuIT23UOnjxwgQEki3//wK0/RKmXeOYPHbYhregyfu1:19122:0:99999:7:::"
-			regex := "s|^" + userName + ":.*|" + userShadow + "|"
-			err = exec.Command("sudo", "sed", "-i", regex, "/etc/shadow").Run()
-			Expect(err).To(Not(HaveOccurred()))
+			createUser()
 
 			// Check a working connection
 			_, err = client.RunSSH(testCmd)
@@ -144,18 +149,10 @@ var _ = Describe("Tools tests", func() {
 				Password: "testpassword",
 			}
 			// Add 'testuser' back
-			err := exec.Command("sudo", "useradd", userName).Run()
-			Expect(err).To(Not(HaveOccurred()))
-
-			// Use 'sed' instead of 'tools.Sed' because root access
-			// is needed and it's easier with 'sudo'
-			userShadow := userName + ":$6$X7HdGuscUQ.XW6dW$B8rTHpY2bZJKyPebFn20fuj0oiLj3D9L557tTBbZ2ZycuIT23UOnjxwgQEki3//wK0/RKmXeOYPHbYhregyfu1:19122:0:99999:7:::"
-			regex := "s|^" + userName + ":.*|" + userShadow + "|"
-			err = exec.Command("sudo", "sed", "-i", regex, "/etc/shadow").Run()
-			Expect(err).To(Not(HaveOccurred()))
+			createUser()
 
 			// Check a working copy
-			err = client.GetFile("../README.md", "/tmp/file.tst", 0644)
+			err := client.GetFile("../README.md", "/tmp/file.tst", 0644)
 			Expect(err).To(Not(HaveOccurred()))
 
 			// Check a non-working copy (bad dst)
