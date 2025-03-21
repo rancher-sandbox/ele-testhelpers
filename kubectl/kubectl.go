1bf1afcb28e9dda111b29ee0dc1d1dba2cc2a1d2
Satyajit Bulage <satyajit.bulage@suse.com>
2025-03-21 13:58:35 +0530

    Fixed linter and update license year
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>

--- kubectl/kubectl.go
+++ kubectl/kubectl.go
@@ -1,7 +1,7 @@
 //nolint:goheader
 /*
 Copyright © 2021 Cloudfoundry (https://github.com/cloudfoundry-incubator/quarks-utils)
-Copyright © 2022 - 2024 SUSE LLC
+Copyright © 2022 - 2025 SUSE LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
